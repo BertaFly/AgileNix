a9c6f141dabbf18ace525885ec0acb0df05f7954
bertafly <irrina.novikova@gmail.com>
2018-11-23 11:50:30 +0200

    Updates

---
+++ precache-manifest.2abd00230016f01f6ae00bf5e2174cd3.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/AgileNix/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "4e07c6290ff319dc992d",
+    "url": "/AgileNix/static/css/main.c961fad4.chunk.css"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/AgileNix/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "fe06200d3e1c7ae9725f",
+    "url": "/AgileNix/static/js/1.fe06200d.chunk.js"
+  },
+  {
+    "revision": "7c5f517e77ccf42b08ab",
+    "url": "/AgileNix/static/js/runtime~main.7c5f517e.js"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/AgileNix/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/AgileNix/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/AgileNix/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/AgileNix/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/AgileNix/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/AgileNix/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "4e07c6290ff319dc992d",
+    "url": "/AgileNix/static/js/main.4e07c629.chunk.js"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/AgileNix/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/AgileNix/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/AgileNix/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/AgileNix/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/AgileNix/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/AgileNix/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/AgileNix/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/AgileNix/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "fe06200d3e1c7ae9725f",
+    "url": "/AgileNix/static/css/1.de76f937.chunk.css"
+  },
+  {
+    "revision": "c5d09147d8e1bc0b4f34dd3aabdb8cad",
+    "url": "/AgileNix/index.html"
+  }
+];
